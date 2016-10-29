5e936ff53333643d013e5fcfd78d85ab966b7ff1
Steve <ljjdbd123@hotmail.com>
2016-10-29 22:59:33 +0800

    regulate name of variables and functions

--- 36kr_newsflash_reader.py
+++ 36kr_newsflash_reader.py
@@ -6,13 +6,14 @@ import json
 req = request.Request('http://new.36kr.com/newsflashes.json')
 req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36')
 
-with request.urlopen(req) as f:
-    print('Status:', f.status, f.reason + '\n')
-    if(200 == f.status):
-        data = f.read()
-        json_data = json.loads(data.decode('utf-8'))
-    else:
-        print('Status Error!\n')
+resp = request.urlopen(req)
+print('Status:', resp.status, resp.reason + '\n')
+if(200 == resp.status):
+    data = resp.read().decode('utf-8')
+    json_data = json.loads(data)
+else:
+    print('Status Error!\n')
+    exit(0)
 
 for i in range(10):
     print(json_data["props"]["newsflashList|newsflash"][i]['title']+'\n')
--- 36kr_newsflash_reader_line_by_line.py
+++ 36kr_newsflash_reader_line_by_line.py
@@ -3,36 +3,37 @@
 from urllib import request
 import json
 
-first_page = 'http://36kr.com/api/newsflash'
+first_page_url = 'http://36kr.com/api/newsflash'
 
-def get_news(url=first_page):
+def get_json_data(url=first_page_url):
 	req = request.Request(url)
 	req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36')
-	with request.urlopen(req) as f:
-		print('Status:', f.status, f.reason + '\n')
-		if(200 == f.status):
-			data = f.read()
-			json_data = json.loads(data.decode('utf-8', 'ignore'))
-			return json_data
-		else:
-			print('Status Error\n')
-			return 'Status Error'
+	resp = request.urlopen(req)
+	print('Status:', resp.status, resp.reason + '\n')
+	if(200 == resp.status):
+		data = resp.read().decode('utf-8', 'ignore')
+		return json.loads(data)
+	else:
+		print('Status Error!\n')
+		exit(0)
 
-def print_news(json_data):
+def get_news(json_data):
 	while True:
-		for i in json_data['data']['items']:
-			s = input('\n' + i['title'])
-			if s == 'q':
-				print('\nDone\n')
+		for news in json_data['data']['items']:
+			us_input = input('\n' + news['title'])
+			if 'q' == us_input:
+				print('\nDone!\n')
 				return 1
-			elif s == 'd':
-				print(i['updated_at'] + '\n' + i['description'] + '\n\n\n')
-			news_id = i['id']
+			elif 'd' == us_input:
+				print(news['updated_at'] + '\n' + news['description'] + '\n\n')
+			else:
+				pass
+			news_id = news['id']
 
 		print('\n--------- Next Page ---------')
-		new_page_url = first_page + '?b_id=' + str(news_id) + '&d=mext'
-		json_data = get_news(new_page_url)
+		new_page_url = first_page_url + '?b_id=' + str(news_id) + '&d=mext'
+		json_data = get_json_data(new_page_url)
 
 
-json_data = get_news()
-print_news(json_data)
+get_news(get_json_data())
+
